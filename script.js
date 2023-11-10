f303c73fd35fe01a375b497d49ed26a0f1767c49
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2023-11-10 22:20:57 +0200

    final commit

--- script.js
+++ script.js
@@ -1,106 +1,137 @@
 const taskInput = document.getElementById('task-input');
 const addTaskButton = document.getElementById('add-task-button');
 const taskList = document.querySelector('.task-list');
-const noTasksContainer = document.createElement('div'); // Container for "No tasks to do" message
+const searchInput = document.getElementById('search-input');
+
+function createIcon(iconClass) {
+    const icon = document.createElement('i');
+    icon.classList.add('fas', iconClass, 'icon');
+    icon.style.cursor = 'pointer';
+    return icon;
+}
+
+function addTask(taskText) {
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('task-item');
+    taskItem.setAttribute('draggable', 'true');
+
+    const taskTextContainer = document.createElement('span');
+    taskTextContainer.classList.add('task-text');
+    taskTextContainer.textContent = taskText;
+
+    const editIcon = createIcon('fa-pen');
+    editIcon.addEventListener('click', function () {
+        const newText = prompt('Edit the task:', taskTextContainer.textContent);
+        if (newText !== null) {
+            taskTextContainer.textContent = newText;
+            saveTasksToLocalStorage();
+        }
+    });
+
+    const space = document.createTextNode(' ');
+
+    const deleteIcon = createIcon('fa-trash-alt');
+    deleteIcon.addEventListener('click', function () {
+        taskItem.remove();
+        saveTasksToLocalStorage();
+        updateNoTasksMessage();
+    });
+
+    const completedIcon = createIcon('fa-check-circle');
+    completedIcon.addEventListener('click', function () {
+        if (taskTextContainer.style.textDecoration === 'line-through') {
+            taskTextContainer.style.textDecoration = 'none';
+        } else {
+            taskTextContainer.style.textDecoration = 'line-through';
+        }
+    });
+
+    const iconsContainer = document.createElement('div');
+    iconsContainer.classList.add('icons-container');
+    iconsContainer.style.display = 'flex';
+    iconsContainer.appendChild(deleteIcon);
+    iconsContainer.appendChild(space);
+    iconsContainer.appendChild(editIcon);
+    iconsContainer.appendChild(space);
+    iconsContainer.appendChild(completedIcon);
+
+    taskItem.appendChild(taskTextContainer);
+    taskItem.appendChild(iconsContainer);
+
+    taskList.appendChild(taskItem);
+    updateNoTasksMessage();
+}
 
 addTaskButton.addEventListener('click', function () {
     const taskText = taskInput.value.trim();
 
     if (taskText !== '') {
-        const taskItem = document.createElement('div');
-        taskItem.classList.add('task-item');
-        taskItem.setAttribute('draggable', 'true');
-        taskItem.textContent = taskText;
-
-        taskList.appendChild(taskItem);
-
+        addTask(taskText);
+        saveTasksToLocalStorage();
         taskInput.value = '';
-
-        // Hide "No tasks to do" message if tasks are added
-        noTasksContainer.style.display = 'none';
     } else {
-        alert('Please enter a task.'); // You can replace this with a more user-friendly notification
+        alert('Please enter a task.');
     }
-
-    updateNoTasksMessage(); // Check if there are tasks after adding
-});
-
-taskInput.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-        addTaskButton.click();
-    }
-});
-
-let dragSrcElement = null;
-
-taskList.addEventListener('dragstart', function (e) {
-    dragSrcElement = e.target;
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', e.target.innerHTML);
-});
-
-taskList.addEventListener('dragover', function (e) {
-    if (e.preventDefault) {
-        e.preventDefault();
-    }
-    return false;
-});
-
-taskList.addEventListener('drop', function (e) {
-    if (e.stopPropagation) {
-        e.stopPropagation();
-    }
-
-    if (dragSrcElement !== e.target) {
-        dragSrcElement.innerHTML = e.target.innerHTML;
-        e.target.innerHTML = e.dataTransfer.getData('text/html');
-    }
-
-    return false;
 });
 
 // Function to update "No tasks to do" message
 function updateNoTasksMessage() {
+    const noTasksMessage = document.querySelector('.no-tasks-message');
+
     if (taskList.children.length === 0) {
-        noTasksContainer.textContent = 'No tasks to do';
-        noTasksContainer.classList.add('no-tasks-message');
-        taskList.appendChild(noTasksContainer);
+        if (!noTasksMessage) {
+            const newNoTasksContainer = document.createElement('div');
+            newNoTasksContainer.textContent = 'No tasks to do';
+            newNoTasksContainer.classList.add('no-tasks-message');
+            taskList.appendChild(newNoTasksContainer);
+        }
     } else {
-        noTasksContainer.style.display = 'none';
+        if (noTasksMessage) {
+            noTasksMessage.style.display = 'none';
+        }
     }
 }
 
-// Initial check for tasks when the page loads
 updateNoTasksMessage();
 
-// Add this to your existing JavaScript file
+//search task
 function searchTasks() {
-    const searchInput = document.getElementById('search-input').value.trim().toLowerCase();
+    const searchInputValue = searchInput.value.trim().toLowerCase();
     const taskItems = document.querySelectorAll('.task-item');
 
     taskItems.forEach(function (taskItem) {
-        const taskText = taskItem.textContent.toLowerCase();
-        const indexOfMatch = taskText.indexOf(searchInput);
+        const taskTextContainer = taskItem.querySelector('.task-text');
+        const taskText = taskTextContainer.textContent.toLowerCase();
+        const indexOfMatch = taskText.indexOf(searchInputValue);
+
+        taskTextContainer.innerHTML = taskText; // Revert the text
 
         if (indexOfMatch !== -1) {
-            const highlightedText = taskText.substring(0, indexOfMatch) +
-                '<span class="highlight">' + taskText.substring(indexOfMatch, indexOfMatch + searchInput.length) + '</span>' +
-                taskText.substring(indexOfMatch + searchInput.length);
-
-            taskItem.innerHTML = highlightedText;
-        } else {
-            taskItem.innerHTML = taskText;
+            const newText = taskText.substring(0, indexOfMatch) +
+                '<span class="highlight">' + taskText.substring(indexOfMatch, indexOfMatch + searchInputValue.length) + '</span>' +
+                taskText.substring(indexOfMatch + searchInputValue.length);
+            taskTextContainer.innerHTML = newText; // Update text with highlighting
         }
     });
 }
 
-// Optional: Clear search input and remove highlighting when the input is cleared
-document.getElementById('search-input').addEventListener('input', function () {
-    if (this.value.trim() === '') {
-        const taskItems = document.querySelectorAll('.task-item');
-        taskItems.forEach(function (taskItem) {
-            taskItem.innerHTML = taskItem.textContent;
-        });
-    }
-});
+searchInput.addEventListener('input', searchTasks);
 
+// Function to save tasks to localStorage
+function saveTasksToLocalStorage() {
+    const tasks = [];
+    taskList.querySelectorAll('.task-item').forEach(taskItem => {
+        tasks.push(taskItem.textContent);
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Function to load tasks from localStorage
+function loadTasksFromLocalStorage() {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    if (tasks) {
+        tasks.forEach(taskText => addTask(taskText));
+    }
+}
+// Call loadTasksFromLocalStorage to load saved tasks on page load
+loadTasksFromLocalStorage();
